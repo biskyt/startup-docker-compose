e57993201e0851b5141b2ce65c88f038038619f7
biskyt <github@gettheitguy.com>
2018-07-06 16:17:41 +0100

    fixed typo and permission in install script

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,6 +3,6 @@
 sudo cp startup-compose.sh /usr/local/bin/
 sudo chmod a+rx /usr/local/bin/startup-compose.sh
 sudo cp startup-compose-cronjob /etc/cron.d/
-sudo chmod a+rx /etc/cron.d/startup-compose-cronjob
+sudo chmod 644 /etc/cron.d/startup-compose-cronjob
 
-echo "Installed, will run at reboot - edit /etc/crontab.d/startup-compose-cronjob to change root"
+echo "Installed, will run at reboot - edit /etc/cron.d/startup-compose-cronjob to change root"
